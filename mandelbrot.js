a69543e22538daab506ad402a7c09cac9e96eb5c
ricardopresto <ricardopresto5@gmail.com>
2019-03-14 18:00:45 +0100

    outline

---
+++ mandelbrot.js
@@ -0,0 +1,13 @@
+let canvas = document.getElementById("frac");
+canvas.width = 680;
+canvas.height = 680;
+
+frac = canvas.getContext("2d");
+
+for (x = 0; x < 680; x++) {
+  for (y = 0; y < 680; y++) {
+    // mandelbrot(x,y);
+  }
+}
+
+// function mandelbrot(x,y) {}
