db65fee63d55dc869ab44f3c7263dcc551cb631b
ricardopresto <ricardopresto5@gmail.com>
2019-06-05 19:32:27 +0200

    draggable selection

--- mandelbrot.js
+++ mandelbrot.js
@@ -9,13 +9,24 @@ overlay.height = size;
 frac = canvas.getContext("2d");
 selection = overlay.getContext("2d");
 
+let renderSel = document.getElementById("renderSel");
+let backBtn = document.getElementById("back");
+let iterDisplay = document.getElementById("iterDisplay");
+let iterSlider = document.getElementById("iterSlider");
+let fringeSlider = document.getElementById("fringeSlider");
+let fringeDisplay = document.getElementById("fringeDisplay");
+
 let iterations = 600;
 let selWidth = canvas.width;
 let selHeight = canvas.height;
 let x1 = 0;
 let y1 = 0;
+let boxPositionX, boxPositionY;
 let count = 0;
 let renderUpdate = false;
+let drawingBox = false;
+let dragging = false;
+let dontclick = false;
 
 let renderList = [
   {
@@ -45,26 +56,19 @@ function mandelbrotCheck(x, y) {
 }
 
 function updateRenderList() {
-<<<<<<< HEAD
-  let newRender = {
-    selectionWidth:
-      selWidth * (renderList[count].selectionWidth / canvas.width),
-    selectionHeight:
-      selHeight * (renderList[count].selectionHeight / canvas.height),
-    selectionX:
-      renderList[count].selectionX +
-      x1 * (renderList[count].selectionWidth / canvas.width),
-    selectionY:
-      renderList[count].selectionY +
-      y1 * (renderList[count].selectionHeight / canvas.height)
-  };
-  renderList.push(newRender);
-  count++;
-=======
   if (renderUpdate) {
+    if (squareSizeY < 0) {
+      y1 = y1 - squareSizeY;
+      squareSizeY = Math.abs(squareSizeY);
+    }
+    if (squareSizeX < 0) {
+      x1 = x1 - squareSizeX;
+      squareSizeX = Math.abs(squareSizeX);
+    }
+
     let newRender = {
-      selectionWidth: selWidth * (renderList[count].selectionWidth / size),
-      selectionHeight: selHeight * (renderList[count].selectionHeight / size),
+      selectionWidth: squareSizeX * (renderList[count].selectionWidth / size),
+      selectionHeight: squareSizeY * (renderList[count].selectionHeight / size),
       selectionX:
         renderList[count].selectionX +
         x1 * (renderList[count].selectionWidth / size),
@@ -77,26 +81,15 @@ function updateRenderList() {
     backBtn.disabled = false;
   }
   renderUpdate = false;
->>>>>>> 21f7b150587ed5aca05a65ea98534170f39b0c73
   renderSelection();
 }
 
 function renderSelection() {
-  console.log(renderList[count]);
   frac.clearRect(0, 0, canvas.width, canvas.height);
   selection.clearRect(0, 0, overlay.width, overlay.height);
 
   let x = 0;
   setInterval(function() {
-<<<<<<< HEAD
-    if (x < canvas.width) {
-      for (let y = 0; y < canvas.height; y++) {
-        let belongsToSet = mandelbrotCheck(
-          renderList[count].selectionX +
-            x * (renderList[count].selectionWidth / canvas.width),
-          renderList[count].selectionY +
-            y * (renderList[count].selectionHeight / canvas.height)
-=======
     if (x < size) {
       for (let y = 0; y < size; y++) {
         let belongsToSet = mandelbrotCheck(
@@ -104,7 +97,6 @@ function renderSelection() {
             x * (renderList[count].selectionWidth / size),
           renderList[count].selectionY +
             y * (renderList[count].selectionHeight / size)
->>>>>>> 21f7b150587ed5aca05a65ea98534170f39b0c73
         );
         if (belongsToSet == iterations) {
           frac.fillStyle = "#000";
@@ -129,14 +121,18 @@ function goBack() {
 renderSelection();
 
 overlay.addEventListener("click", canvasClick);
-overlay.addEventListener("mousemove", drawSelection);
+overlay.addEventListener("mousemove", mouseMove);
+overlay.addEventListener("mousedown", mouseDown);
+overlay.addEventListener("mouseup", mouseUp);
 
-let drawingBox = false;
-
-function canvasClick(event) {
+function canvasClick(e) {
   let rect = overlay.getBoundingClientRect();
-  let x = event.clientX - rect.left - 2;
-  let y = event.clientY - rect.top - 2;
+  let x = e.clientX - rect.left - 2;
+  let y = e.clientY - rect.top - 2;
+  if (dontclick == true) {
+    dontclick = false;
+    return;
+  }
   if (drawingBox == false) {
     x1 = x;
     y1 = y;
@@ -147,21 +143,73 @@ function canvasClick(event) {
   }
 }
 
-function drawSelection(event) {
+function mouseMove(e) {
+  if (drawingBox == true) {
+    drawSelection(e);
+  } else if (dragging == true) {
+    dragBox(e);
+  }
+}
+
+function mouseDown(e) {
+  let rect = overlay.getBoundingClientRect();
+  let mouseX = e.clientX - rect.left;
+  let mouseY = e.clientY - rect.top;
+
+  if (selection.getImageData(mouseX, mouseY, 1, 1).data[3] == 92) {
+    dragging = true;
+    boxPositionX = mouseX - x1;
+    boxPositionY = mouseY - y1;
+  }
+}
+
+function mouseUp() {
+  if (dragging == true) {
+    dontclick = true;
+    dragging = false;
+  }
+}
+
+function dragBox(e) {
+  selection.clearRect(0, 0, overlay.width, overlay.height);
+
+  let rect = overlay.getBoundingClientRect();
+  let mouseX = e.clientX - rect.left;
+  let mouseY = e.clientY - rect.top;
+
+  x1 = mouseX - boxPositionX;
+  y1 = mouseY - boxPositionY;
+  selection.fillStyle = "rgba(0, 0, 255, 0.36)";
+  selection.fillRect(x1, y1, squareSizeX, squareSizeY);
+}
+
+function drawSelection(e) {
   if (drawingBox == true) {
     selection.clearRect(0, 0, overlay.width, overlay.height);
     let rect = overlay.getBoundingClientRect();
-    let mouseX = event.clientX - rect.left;
-    let mouseY = event.clientY - rect.top;
-    selection.fillStyle = "rgba(0, 0, 255, 0.3)";
+    let mouseX = e.clientX - rect.left;
+    let mouseY = e.clientY - rect.top;
     selWidth = mouseX - x1;
     selHeight = mouseY - y1;
 
-    if (selWidth < 0 || selHeight < 0) return;
-    if (selWidth > selHeight) selHeight = selWidth;
-    if (selHeight > selWidth) selWidth = selHeight;
-
+    selection.fillStyle = "rgba(0, 0, 255, 0.2)";
     selection.fillRect(x1, y1, selWidth, selHeight);
+    selection.fillStyle = "rgba(0, 0, 255, 0.2)";
+    if (selWidth > 0 && selHeight > 0) {
+      squareSizeX = squareSizeY = Math.min(selWidth, selHeight);
+    }
+    if (selWidth < 0 && selHeight < 0) {
+      squareSizeX = squareSizeY = Math.max(selWidth, selHeight);
+    }
+    if (selWidth > 0 && selHeight < 0) {
+      squareSizeX = Math.min(selWidth, Math.abs(selHeight));
+      squareSizeY = -squareSizeX;
+    }
+    if (selWidth < 0 && selHeight > 0) {
+      squareSizeY = Math.min(selHeight, Math.abs(selWidth));
+      squareSizeX = -squareSizeY;
+    }
+    selection.fillRect(x1, y1, squareSizeX, squareSizeY);
   }
 }
 
@@ -173,13 +221,6 @@ function iterUpdate() {
 function fringeUpdate() {
   fringeDisplay.textContent = `Fringe: ${fringeSlider.value}%`;
 }
-
-let renderSel = document.getElementById("renderSel");
-let backBtn = document.getElementById("back");
-let iterDisplay = document.getElementById("iterDisplay");
-let iterSlider = document.getElementById("iterSlider");
-let fringeSlider = document.getElementById("fringeSlider");
-let fringeDisplay = document.getElementById("fringeDisplay");
 
 backBtn.disabled = true;
 
